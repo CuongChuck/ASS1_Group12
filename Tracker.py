650a5172d0a9d137d710f95c2b6e466a36ac1ed5
Quang Mai <maithanhnhatquang1@gmail.com>
2024-10-21 19:55:20 +0700

    Add class Tracker

--- Tracker.py
+++ Tracker.py
@@ -1,1 +1,28 @@
+import socket
+from collections import defaultdict
 
+# Configuration
+CONFIGS = {
+    "constants": {"TRACKER_ADDRESS": ("localhost", 8888), "MAX_CONNECTIONS": 8}
+}
+
+
+class Tracker:
+    def __init__(self):
+        # Dictionary to store which peers own which files
+        self.file_owners_list = defaultdict(list)
+        # Dictionary to track the sending frequency of peers
+        self.send_frequency_list = defaultdict(int)
+
+        # Create and bind the tracker socket using TCP
+        tracker_address = CONFIGS["constants"]["TRACKER_ADDRESS"]
+        max_connections = CONFIGS["constants"]["MAX_CONNECTIONS"]
+
+        self.tracker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tracker_socket.bind(tracker_address)
+        self.tracker_socket.listen(max_connections)
+        print(f"Tracker listening on {tracker_address}")
+
+
+if __name__ == "__main__":
+    tracker = Tracker()
