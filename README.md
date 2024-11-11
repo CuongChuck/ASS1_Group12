3db8a5bfdb7490430f396bc064688b9eeba6b8e4
Quang Mai <maithanhnhatquang1@gmail.com>
2024-11-11 14:40:26 +0700

    Merge implement-put-request to main

--- README.md
+++ README.md
@@ -14,7 +14,35 @@ Build a Simple Torrent-like Application (STA) with the protocols defined by each
 
 ## API
 
+### `GET /`
+
+Peer sends a `GET` request to `/` and receives the response as follows.
+
+```python
+request = (
+    f"GET /?peer_id={peer.id}&peer_ip_address={peer.ip_address}&peer_port={peer.port}&bitfield={peer.bitfield} HTTP/1.1\r\n"
+    "Host: 127.0.0.1:8888\r\n"
+    "User-Agent: CustomClient/1.0\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+)
+```
+
+```python
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: application/json\r\n"
+    "Content-Length: 94\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    "peer id: 123, ip: 127.0.0.1, port: 1234, bitfield: 010110\n"
+    "peer id: 456, ip: 127.0.0.1, port: 1235, bitfield: 011110\n"
+)
+```
+
 ### `GET /announce`
+
+Peer sends a `GET` request to `/announce`, tracker adds peer to peer list and send the response as follows.
 
 ```python
 request = (
@@ -23,5 +51,43 @@ request = (
     "User-Agent: CustomClient/1.0\r\n"
     "Connection: close\r\n"
     "\r\n"
+)
+```
+
+```python
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: application/json\r\n"
+    "Content-Length: 94\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    "peer id: 123, ip: 127.0.0.1, port: 1234, bitfield: 010110\n"
+    "peer id: 456, ip: 127.0.0.1, port: 1235, bitfield: 011110\n"
+)
+```
+
+### `PUT /seeding`
+
+Peer sends a `PUT` request to `/seeding` to update `bitfield` and receives the response as follows.
+
+```python
+request = (
+    f"PUT /seeding?peer_id={peer.id}&peer_ip_address={peer.ip_address}&peer_port={peer.port}&bitfield={peer.bitfield} HTTP/1.1\r\n"
+    "Host: 127.0.0.1:8888\r\n"
+    "User-Agent: CustomClient/1.0\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+)
+```
+
+```python
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: application/json\r\n"
+    "Content-Length: 94\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    "peer id: 123, ip: 127.0.0.1, port: 1234, bitfield: 010110\n"
+    "peer id: 456, ip: 127.0.0.1, port: 1235, bitfield: 011110\n"
 )
 ```
--- tracker.py
+++ tracker.py
@@ -47,25 +47,26 @@ class Tracker:
             500: "Internal Server Error",
         }
         status_message = status_messages.get(status_code, "OK")
-        response_body = json.dumps(content)
+
+        # Determine if the response should be JSON or plain text
+        if isinstance(content, dict):
+            # JSON response
+            response_body = json.dumps(content)
+            content_type = "application/json"
+        else:
+            # Plain text response
+            response_body = content
+            content_type = "text/plain"
+
         response = (
             f"HTTP/1.1 {status_code} {status_message}\r\n"
-            "Content-Type: application/json\r\n"
+            f"Content-Type: {content_type}\r\n"
             f"Content-Length: {len(response_body)}\r\n"
             "Connection: close\r\n"
             "\r\n"
             f"{response_body}"
         )
         conn.sendall(response.encode())
-
-    # def register_peer(self, peer_id, addr, conn):
-    #     """Registers a peer and sends a success response."""
-    #     if peer_id:
-    #         self.peers[peer_id] = {"addr": addr, "online": True}
-    #         print(f"Registered peer: {peer_id} at {addr}")
-
-    #         # Send registration success response
-    #         self.send_message(conn, {"success": True})
 
     def register_peer(self, id, ip, port, bitfield):
         if id:
@@ -79,33 +80,17 @@ class Tracker:
                 f"Registered peer: {id} at {ip}:{port} with bitfield {bitfield}."
             )
 
-    # def handle_peer(self, conn, addr):
-    #     """Handle individual peer connections."""
-    #     print(f"Connected to peer: {addr}")
-    #     try:
-    #         message = self.receive_message(conn)
-    #         if not message:
-    #             return
-
-    #         action = message.get("action")
-
-    #         if action == CONFIGS["ACTIONS"]["REGISTER"]:
-    #             peer_id = message.get("peer_id")
-    #             self.register_peer(peer_id, addr, conn)
-    #         else:
-    #             self.send_message({"error": "Invalid action"})
-
-    #     except Exception as e:
-    #         print(f"Error handling peer: {e}")
-    #     finally:
-    #         conn.close()
-
     def handle_peer(self, conn, addr):
         """Handle individual peer connections."""
         try:
             request = conn.recv(CONFIGS["BUFFER_SIZE"]).decode()
+            print(f"--------------------------------------------------")
+            print(f"--------------------------------------------------")
+            print(f"Request: {request}")
             if request.startswith("GET"):
                 self.handle_get_request(conn, request)
+            elif request.startswith("PUT"):
+                self.handle_put_request(conn, request)
             else:
                 self.send_http_response(
                     conn, 400, {"error": "Unsupported request"}
@@ -139,8 +124,9 @@ class Tracker:
             peer_ip = params.get("peer_ip_address")
             peer_port = params.get("peer_port")
             bitfield = params.get("bitfield")
+
             print(
-                f"id: {peer_id}, ip: {peer_ip}, port: {peer_port}, bitfield: {bitfield}"
+                f"Request from peer id = {peer_id} at {peer_ip}:{peer_port}, bitfield = {bitfield}:"
             )
 
             # Check if any required parameters are missing
@@ -154,19 +140,73 @@ class Tracker:
                 # Register the peer with extracted data
                 self.register_peer(peer_id, peer_ip, peer_port, bitfield)
 
-            peer_list = [
-                {
-                    "id": id,
-                    "ip": details["ip"],
-                    "port": details["port"],
-                    "bitfield": details["bitfield"],
-                }
+            peer_list_text = "\n".join(
+                f"peer id: {id}, ip: {details['ip']}, port: {details['port']}, bitfield: {details['bitfield']}"
                 for id, details in self.peers.items()
-            ]
-            self.send_http_response(conn, 200, peer_list)
+            )
+            print(peer_list_text)
+            self.send_http_response(conn, 200, peer_list_text)
 
         except Exception as e:
             print(f"Error handling GET request: {e}")
+            self.send_http_response(
+                conn, 500, {"error": "Internal Server Error"}
+            )
+
+    def handle_put_request(self, conn, request):
+        try:
+            # Split request to parse PUT parameters
+            request_line = request.splitlines()[0]
+            method, path, _ = request_line.split()
+
+            # Ensure it's an HTTP PUT request
+            if method != "PUT":
+                self.send_http_response(conn, 400, {"error": "Bad Request"})
+                return
+
+            # Extract query parameters from the path
+            query = path.split("?")[1]
+            params = dict(item.split("=") for item in query.split("&"))
+
+            # Retrieve necessary parameters
+            peer_id = params.get("peer_id")
+            peer_ip = params.get("peer_ip_address")
+            peer_port = params.get("peer_port")
+            bitfield = params.get("bitfield")
+
+            print(
+                f"Request from peer id = {peer_id} at {peer_ip}:{peer_port}, bitfield = {bitfield}:"
+            )
+
+            # Check if any required parameters are missing
+            if not peer_id or not peer_ip or not peer_port or not bitfield:
+                self.send_http_response(
+                    conn, 400, {"error": "Bad Request, missing parameters"}
+                )
+                return
+
+            if "seeding" in path:
+                # Check if the peer is already registered
+                if peer_id in self.peers:
+                    # Update the peer's bitfield
+                    self.peers[peer_id]["bitfield"] = bitfield
+                    print(f"Updated bitfield for peer {peer_id} to {bitfield}")
+                else:
+                    # Peer is not found
+                    self.send_http_response(
+                        conn,
+                        400,
+                        {"error": "Peer not found, cannot update bitfield"},
+                    )
+                    return
+
+            peer_list_text = "\n".join(
+                f"peer id: {id}, ip: {details['ip']}, port: {details['port']}, bitfield: {details['bitfield']}"
+                for id, details in self.peers.items()
+            )
+            self.send_http_response(conn, 200, peer_list_text)
+        except Exception as e:
+            print(f"Error handling PUT request: {e}")
             self.send_http_response(
                 conn, 500, {"error": "Internal Server Error"}
             )
